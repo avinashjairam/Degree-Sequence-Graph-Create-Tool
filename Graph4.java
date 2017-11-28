14f227a8b9f36503a1dfb683b3c3ea3f67d74e73
avinashjairam <avinash.jairam@gmail.com>
2017-11-27 23:03:26 -0500

    This version works with 7 2s

--- Graph4.java
+++ Graph4.java
@@ -4,14 +4,29 @@ import javax.swing.*;
 import java.util.*;
 
 
-public class Graph3 extends JFrame {
-	 public Graph3(){
-	 	add(new NewPanel());
-    	
-    }
+public class Graph4 extends JFrame {
+  NewPanel panel = new NewPanel();
+
+	 public Graph4(){
+	 	add(panel);
+
+    addMouseListener(new MouseAdapter() { 
+          public void mousePressed(MouseEvent e) { 
+            panel.processClicks(e.getX(),e.getY());
+          } 
+    });
+ 
+      addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+              panel.processDrag(e.getX(),e.getY());           
+           }
+     });
+  }
+
 	public static void main(String [] args){
-		Graph3 frame = new Graph3();
-        frame.setSize(800, 600);
+		Graph4 frame = new Graph4();
+        frame.setSize(1200, 1000);
         frame.setTitle("Graphic Sequence");
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -29,62 +44,97 @@ class NewPanel extends JPanel implements ActionListener{
 	
 	int [] sequence;
 	int [] originalSequence;
+  char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 	int [][] matrix;
 
 	int [] xCoordinates;
 	int [] yCoordinates;
 	int graphicCount = 0;
+
+  ArrayList<Vertex> vertex;
+  ArrayList<Edge>edges;
+
+  ArrayList<ArrayList<Vertex>> subVertex = new ArrayList<ArrayList<Vertex>>();
+
+
 	boolean graphic = false;
 	boolean negativeDegree = false;
 	boolean invalid = false;
+  boolean reset = false;
+  boolean attempt = false;
 	int size;
+    int endX=300, endY=400, x=300,y=300,z=200;
+    Color randomColor = new Color(220, 220, 220);
 
 
 	public NewPanel(){
-	 	super.setPreferredSize(new Dimension(600, 400));       
+    super.setPreferredSize(new Dimension(600, 400));     
+    vertex = new ArrayList<Vertex>();  
+    edges = new ArrayList<Edge>();
         panel2.add(txtdata);
         panel2.add(enter);
-       // panel3.add(new NewPanel());
         panel1.add(panel2, BorderLayout.EAST);
         panel1.add(panel3, BorderLayout.CENTER);
         enter.addActionListener(this);
+  
         add(panel1);
 	}
 
 	protected void paintComponent(Graphics g){
+    
+
 		super.paintComponent(g);
 		
-		int endX=300, endY=200, x=300,y=200,z=100;
+		// int endX=300, endY=400, x=300,y=300,z=200;
+
+    System.out.println("Graphic is " + graphic);
+
+    if(reset){
+             g.setColor(UIManager.getColor ( "Panel.background" ));
+             g.fillRect(0,0,1200,1000);
+             reset=false;
+              g.setColor(randomColor);
+
+          
+     }
+
   	
   		if(graphic){
+                    g.setColor(new Color(0,0,0));
+
+         for(int i=0; i <edges.size();i++){
+          for(int j=0; j< edges.get(i).getNeighborsSize();j++){
+            g.drawLine(edges.get(i).getX(),edges.get(i).getY()+10,edges.get(i).neighbors.get(j).getXCoordinates(),edges.get(i).neighbors.get(j).getYCoordinates()+10);
+            System.out.println(edges.get(i).getName() + " to " + edges.get(i).neighbors.get(j).getName());
+          }
+       }
+      
   			for ( int i = 0; i<size; i++ ){
      			endX = x + (int)(z*Math.cos( (2*Math.PI/size)*i ));
       			endY = y - (int)(z*Math.sin( (2*Math.PI/size)*i ));  // Note "-"
       			xCoordinates[i] = endX +10;
       			yCoordinates[i] = endY +10;
-      			System.out.println("xCoordinates " + endX + "yCoordinates " + endY);
+      			//System.out.println("xCoordinates " + endX + "yCoordinates " + endY);
+            g.setColor(randomColor);
 		        g.fillOval(endX,endY,20,20);
-		      //  g.drawString(Integer.toString(i),endX,endY);
+            System.out.println(endX + " " + endY + " " + alphabet[i]);
+            g.setColor(new Color(0,0,0));
+            g.drawString(Character.toString(alphabet[i]),endX+5,endY+15);
+            vertex.add(new Vertex(alphabet[i],originalSequence[i], endX+10,endY+10));
+
+           
+       
+
     		}
 
-    		// for(int i =0; i < size; i++){
-    		// 	 g.drawString(Integer.toString(originalSequence[i]),xCoordinates[i]-10, yCoordinates[i]-10);
-
-    		// 	for(int j = 0; j<size;j++){
-    		// 		if(i == j){
-    		// 			continue;
-    		// 		}
-    		// 		if(matrix[i][j]==1){
-    		// 			g.drawLine(xCoordinates[i],yCoordinates[i],xCoordinates[j],yCoordinates[j]);
-
-    		// 		}
-    		// 	}
-    		// }
-    		//g.drawLine(xCoordinates[1],yCoordinates[1],xCoordinates[4],yCoordinates[4]);
-
+     
+    	
     	}
 
+    
     	if(invalid){
+        g.setColor(new Color(0,0,0));
+
     		g.drawString(data,400,300);
     	}
 
@@ -94,18 +144,90 @@ class NewPanel extends JPanel implements ActionListener{
 		
 	}
 
+  public void processClicks(int x, int y){
+    System.out.println("x = " + x + "y =" + y);
+
+    for(int i = 0; i < xCoordinates.length;i++){
+      if((Math.abs(xCoordinates[i] - x) < 50) && (Math.abs(yCoordinates[i] - y) < 50) ){
+        System.out.println(alphabet[i]);
+      }
+    }
+  }
+
+  public void processDrag(int x, int y){
+    char v ='4';
+
+    for(int i = 0; i < xCoordinates.length;i++){
+      if((Math.abs(xCoordinates[i] - x) < 50) && (Math.abs(yCoordinates[i] - y) < 50) ){
+        v = (alphabet[i]);
+      }
+
+      for(int j = 0; j < edges.size();j++){
+        if(edges.get(j).getName()==v){
+          edges.get(j).setX(x);
+          edges.get(j).setY(y);
+          endX=x;
+          endY=y;
+          repaint();
+        }
+      }
+    }
+  }
+
 	 public void actionPerformed(ActionEvent e){
         if (e.getSource() == enter) {
-             data = txtdata.getText(); //perform your operation
+              if(attempt){
+                reset=true;
+
+                data="";     
+  
+               sequence=null;
+                originalSequence=null;
+               matrix =null;
+
+               xCoordinates =null;
+              yCoordinates =null;
+              graphicCount = 0;
+
+             vertex = new ArrayList<Vertex>();  
+            edges = new ArrayList<Edge>();
+
+            subVertex = new ArrayList<ArrayList<Vertex>>();
+             graphic = false;
+             negativeDegree = false;
+            invalid = false;
+               size=0;
+                 data = txtdata.getText(); //perform your operation
              data = data.replace(",","");
              data = data.trim();
              initializeSequence();
              processSequence();
+              //  this.dispose();
+               // new Graph3();
+                repaint();
+              }else{
+                 data = txtdata.getText(); //perform your operation
+                  data = data.replace(",","");
+                  data = data.trim();
+                  initializeSequence();
+                   processSequence();
+
+
+              }
+
+            
+         //    findEdges();
+                // System.out.println(vertex.size());
+
+            
+
            // repaint();
         }
     }
 
     public void initializeSequence(){
+      int endX=300, endY=400, x=300,y=300,z=200;
+
     	sequence = new int[data.length()];
     	originalSequence = new int[data.length()];
     	xCoordinates = new int[data.length()];
@@ -114,14 +236,23 @@ class NewPanel extends JPanel implements ActionListener{
     	for(int i = 0; i < sequence.length;i++){
     		sequence[i] = Character.getNumericValue(data.charAt(i));
     		originalSequence[i] = Character.getNumericValue(data.charAt(i));
-    		//System.out.println(sequence[i]);
+
+        endX = x + (int)(z*Math.cos( (2*Math.PI/sequence.length)*i ));
+        endY = y - (int)(z*Math.sin( (2*Math.PI/sequence.length)*i ));  // Note "-"
+        vertex.add(new Vertex(alphabet[i], originalSequence[i], endX+10,endY+10));
+
+
     	}
-    	System.out.print("after init");
-    	printSequence();
+
+       
     }
 
     public void processSequence(){
+     Collections.sort(vertex, (v1, v2) -> v2.getDegree() - v1.getDegree());
+     // findEdges();
+     int count =0;
     	size = sequence.length;
+      attempt = true;
     	while(graphic == false){
     		int front = sequence[0];
     		graphicCount =0;
@@ -135,17 +266,24 @@ class NewPanel extends JPanel implements ActionListener{
     			break;
     		}
     		else{
+          ArrayList<Vertex> sub = new ArrayList<Vertex>();
     			for(int i = 0; i < front;i++){
     				sequence[i] -=1;
+            sub.add(new Vertex(alphabet[i], sequence[i],0,0));
     			}
+
+          subVertex.add(sub);
+          Collections.sort(subVertex.get(count), (v1, v2) -> v2.getDegree() - v1.getDegree());    
+
+          count++;
     			Arrays.sort(sequence);
     			reverse();
-    			printSequence();
+    			//printSequence();
 
     			for(int i = 0; i <sequence.length; i++){
     				if(sequence[i]==0){
     					graphicCount +=1;
-    					System.out.println ("graphic count " + graphicCount + "length " + sequence.length);
+    				//	System.out.println ("graphic count " + graphicCount + "length " + sequence.length);
     				}
     				if(sequence[i]==-1){
     					negativeDegree = true;
@@ -162,12 +300,18 @@ class NewPanel extends JPanel implements ActionListener{
     			if(graphicCount == sequence.length){
     				graphic =true;
     				data = "valid Sequence";
-    				adjacencyMatrix();
     				//printMatrix();
     				findEdges();
-    				printMatrix();
-    				System.out.println("num of edges " + countEdges(0));
+    			   printEdges();
+    		//		System.out.println("num of edges " + countEdges(0));
     				repaint();
+
+            for(ArrayList<Vertex> v : subVertex){
+              for(int i =0; i<v.size();i++){
+                System.out.print(v.get(i).getName() + " " + v.get(i).getDegree() + " ");
+              }
+              System.out.println();
+            }
     				break;
     			}
 
@@ -193,62 +337,194 @@ class NewPanel extends JPanel implements ActionListener{
 		}
    }
 
-   public void adjacencyMatrix(){
-   		matrix = new int [size][size];
-
-   		for(int i =0; i<size;i++){
-   			for(int j = 0; j<size;j++){
-   				//if((i==j) || (j == 0 && i ==2)){
-   					matrix[i][j]=0;
-   				/*}
-   				else{
-   					matrix[i][j] = 1;
-   				}*/
-   			}
-   		}
-   }
-
-
-   public void printMatrix(){
-   	for(int i =0; i<size;i++){
-   			for(int j = 0; j<size;j++){
-   				System.out.print(matrix[i][j] + " ");
-   			}
-   			System.out.println();
-   		}
-
-   }
-
-   public int countEdges(int col){
-   	int count = 0;
-   	for(int i = 0; i < size; i++){
-   		if(matrix[i][col]==1){
-   			count++;
-   		}
-   	}
-
-   	return count;
-   }
-
   public void findEdges(){
-  	int checks = 0;
-   	for(int i = 0; i < size; i++){
-   		checks =0;
-   		for(int j = 0; j <size; j++){
-   			if(i==j){
-   				continue;
-   			}
-   			else{
-   				if(countEdges(j) < originalSequence[j]){
-   					System.out.println("i =" +i + " j =" +j + "countEdges " + countEdges(j) + " originalSequence " +originalSequence[j]);
-   					if(checks < originalSequence[i]){
-   						matrix[i][j]=1;
-   						checks++;
-   					}
-   				}
-   			}
-   		} 
-   }
+    while(!vertex.isEmpty()){
+      Vertex front = vertex.get(0);
+
+      vertex.remove(0);
+
+      System.out.println("front is " + front.getName());
+
+      ArrayList<Vertex>copy = new ArrayList<Vertex>();
+
+      for(int i = 0; i<front.getDegree();i++){
+        copy.add(new Vertex(vertex.get(i).getName(),vertex.get(i).getDegree(), vertex.get(i).getXCoordinates(),vertex.get(i).getYCoordinates()));
+      }
+
+    //  map.put(front.getName(), new ArrayList<Vertex>(vertex.subList(0,front.getDegree())));
+      Edge edge = new Edge(front, copy);
+      edges.add(edge);
+    
+      //System.out.println("vertex size is " + vertex.size());
+
+      edge.printList();
+
+       System.out.println("Vertex is (Before decrement) ");
+      for(int i =0 ;i <vertex.size();i++){
+        System.out.println(vertex.get(i).getName() + " deg. " + vertex.get(i).getDegree() + " " );
+      }
+      System.out.println();
+
+       System.out.println("Vertex is (During Decrement ");
+
+       for(int i = 0; i <front.getDegree();i++){
+          System.out.println(vertex.get(i).getName() + " deg. " + vertex.get(i).getDegree() + " " );
+
+        // if(i == vertex.size()){
+        //    System.out.println("i =" + i + " vertex size" + vertex.size());
+        //      vertex.get(i-1).decrement();
+
+        //      // if(vertex.get(i-1).getDegree() == 0){
+        //      //    vertex.remove(i-1);
+        //      //  }
+        //      removeZero();
+
+
+        //      break;
+        // }
+        System.out.print(vertex.get(i).getName());
+            vertex.get(i).decrement();
+
+              if(vertex.get(i).getDegree() == 0){
+              //  vertex.remove(i);
+               // removeZero();
+              }
+        
+      }
+//      Collections.shuffle(vertex);
+
+      Collections.sort(vertex, (v1, v2) -> v2.getDegree() - v1.getDegree());    
+
+      System.out.print("Vertex is (After Decrement) ");
+      for(int i =0 ;i <vertex.size();i++){
+        System.out.println(vertex.get(i).getName() + " deg. " + vertex.get(i).getDegree() + " " );
+      }
+      System.out.println();
+    }
+  
+
  }
+
+ public void removeZero(){
+  for(int i =0;i<vertex.size();i++){
+        //  System.out.println("Vertex" + vertex.get(i).getName() + " " + vertex.get(i).getDegree());
+
+    if(vertex.get(i).getDegree()==0){
+      System.out.println(" Removing Vertex " + vertex.get(i).getName() + " with deg. " + vertex.get(i).getDegree());
+      vertex.remove(i);
+    }
+  }
+ }
+
+ public void printEdges(){
+  for(int i =0; i <edges.size();i++){
+    System.out.print("EDGES " +edges.get(i).getName()+ " ");
+    edges.get(i).printList();
+  }
+ }
+
+
+
+
+
+
+
+}
+
+class Vertex{
+  char name;
+  int degree;
+  int xCoordinates;
+  int yCoordinates;
+
+  public Vertex(char name, int degree, int xCoordinates, int yCoordinates){
+    this.name = name;
+    this.degree = degree;
+    this.xCoordinates = xCoordinates;
+    this.yCoordinates = yCoordinates;
+  }
+
+  public void setName(char name){
+    this.name=name;
+  }
+
+  public void setDegree(int degree){
+    this.degree=degree;
+  }
+
+  public void setXCoordinates(int xCoordinates){
+    this.xCoordinates=xCoordinates;
+  }
+
+  public void setYCoordinates(int yCoordinates){
+    this.yCoordinates=yCoordinates;
+  }
+
+  public void decrement(){
+    this.degree--;
+  }
+
+  public char getName(){
+    return this.name;
+  }
+
+  public int getDegree(){
+    return this.degree;
+  }
+
+  public int getXCoordinates(){
+    return this.xCoordinates;
+  }
+
+  public int getYCoordinates(){
+    return this.yCoordinates;
+  }
+
+}
+
+class Edge{
+  public Vertex vertex;
+  public ArrayList<Vertex>neighbors = new ArrayList<Vertex>();
+
+  public Edge(Vertex v, ArrayList<Vertex> neighbors){
+    this.vertex = v;
+    this.neighbors = neighbors;
+  }
+
+  public void printList(){
+    for(int i =0; i<neighbors.size();i++){
+      System.out.print(neighbors.get(i).getDegree() + " ");
+    }
+
+    System.out.println();
+  }
+
+  public void setX(int x){
+    vertex.setXCoordinates(x);
+  }
+
+  public void setY(int y){
+    vertex.setYCoordinates(y);
+  }
+
+  public char getName(){
+    return vertex.getName();
+  }
+
+  public int getDegree(){
+    return vertex.getDegree();
+  }
+
+  public int getX(){
+    return vertex.getXCoordinates();
+  }
+
+  public int getY(){
+    return vertex.getYCoordinates();
+  }
+
+  public int getNeighborsSize(){
+    return neighbors.size();
+  }
 
 }
